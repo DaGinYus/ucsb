36eab899027ad0090f0594f7448f28c645c47f1c
Matthew Wong <matthew@adveca.com>
2022-01-26 23:07:22 -0800

    yay it finally works

--- phys129L/hw4/4_factoring.py
+++ phys129L/hw4/4_factoring.py
@@ -10,12 +10,12 @@ import random
 import numpy as np
 
 
-def isprime(num, k=20):
+def isprime(num, k=40):
     """Implementation of Rabin-Miller primality test.
     See Wikipedia article for details.
     
     Args:
-        n: a positive integer to test for primality.
+        num: a positive integer to test for primality.
         k: the number of rounds of testing.    
 
     Returns:
@@ -29,27 +29,67 @@ def isprime(num, k=20):
     # rewrite n as (2^s)*d + 1 by repeated factoring
     d = num-1
     s = 0
-    while s%2 == 0:
-        d = d//2
+    while d%2 == 0:
+        d //= 2
         s += 1
     # check congruence of random number a^d with 1 or -1 mod n
+    # (Fermat's little theorem)
     for _ in range(k):
-        a = random.randrange(2, num-2)
-        x = a**d % num
+        a = random.randint(2, num-2)
+        x = pow(a, d, num)
         if x == 1 or x == num-1:
             continue
-        for _ in range(s-1):
-            x = x**2 % num
+        for _ in range(s):
+            x = pow(x, 2, num)
             if x == num-1:
-                continue
-        return False
+                break
+        else:
+            return False
+    # probably prime
     return True
+
+def pollardrho(num, count=0):
+    """Implementation of Pollard's rho algorithm.
+    
+    Args:
+        num: an odd number greater than 3 to be factorized.
+        count: a number used to count how many times we've retried.
+            If it's greater than 0, then the polynomial is incremented
+            and a starting value is randomly generated.
+
+    Returns:
+        d: a factor of num
+"""
+    if isprime(num):
+        return num
+    # starting values
+    tort = 2
+    if count:
+        tort = random.randint(3, num)
+    hare = tort
+    d = 1 # this variable is checked if it is a factor
+    while d == 1:
+        # the polynomial g(x) = (x^2 + 1) mod n is applied
+        # (twice for y)
+        tort = (tort**2 + 1 + count)%num
+        hare = (hare**2 + 1 + count)%num
+        hare = (hare**2 + 1 + count)%num
+        d = int(np.gcd(np.abs(tort-hare), num))
+
+        if d == num:
+            # the test failed, try with a different starting value
+            return pollardrho(num, count+1)
+        elif d != 1:
+            # found a factor
+            return d
 
 def factorize(factors, reseed=False):
     """Prime factorizes a number using Pollard's rho algorithm.
     
     This algorithm is faster than trial by division for large numbers.
-    See Wikipedia article for details.
+    Factors of 2 are repeatedly divided first, leaving an odd number to
+    factorize. Then the function is repeatedly called over the running
+    list of factors until they are all prime.
 
     Args:
         factors: a list of the current factors, which should only
@@ -59,49 +99,36 @@ def factorize(factors, reseed=False):
     Returns:
         factors: a list of prime integers.
     """
+    # factorize powers of 2 first
+    f0 = factors[0]
+    if f0%2 == 0 and f0 != 2:
+        count = 0
+        while f0%2 == 0 and f0 != 2:
+            f0 //= 2
+            count += 1
+        factors[0:0] = [2 for _ in range(count)]
+        factors[-1] = f0
     # check if the factors are all prime, in which case return
     # otherwise set n to the first composite factor
     all_prime = True
-    print(factors)
-    for i, f in enumerate(factors):
+    for f in factors:
         if not isprime(f):
             # set composite number
             composite = f
             all_prime = False
             break
     if all_prime:
-        print("all prime")
         return factors
 
-    # starting values
-    tort = 2
-    if reseed:
-        tort = random.randrange(3, composite)
-    hare = tort
-    candidate = 1
-    while candidate == 1:
-        # the polynomial g(x) = (x^2 + 1) mod n is applied
-        # (twice for y)
-        tort = (tort**2 + 1)%composite
-        hare = (hare**2 + 1)%composite
-        hare = (hare**2 + 1)%composite
-        candidate = np.gcd(np.abs(tort-hare), composite)
-
-        if candidate == composite:
-            # the test failed, try with a different starting value
-            factorize(factors, True)
-        else:
-            # found a factor, replace f with d and f/d
-            factors[i:i+1] = candidate, composite//candidate
-            factorize(factors)
+    factor = pollardrho(composite)
+    # replace the composite with its factors
+    replace = factors.index(composite)
+    factors[replace:replace+1] = factor, composite//factor
+    return factorize(factors)
 
 
 def main():
-    """Gets the prime factorization of a number.
-
-    Uses NumPy to perform array arithmetic, because it is much faster
-    than looping.
-    """
+    """Gets the prime factorization of a number."""
     while True:
         usrinput = input("Enter an integer: ")
         try:
@@ -116,8 +143,8 @@ def main():
         except ValueError:
             print("Please enter an integer.")
 
-    print(isprime(41))
-    print(factorize([num]))
+    result = factorize([num])
+    print(' '.join([str(n) for n in result]))
     
 
 if __name__ == "__main__":
