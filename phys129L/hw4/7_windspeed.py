7cb9bde4739a8d8a704c79a5e4a888e84f04fd2b
Matthew Wong <matthew@adveca.com>
2022-01-31 15:58:16 -0800

    initial commit of problem 7

---
+++ phys129L/hw4/7_windspeed.py
@@ -0,0 +1,33 @@
+"""
+TKC!
+Wind Speed
+Matthew Wong
+Phys 129L Hw4 Pb7
+2022-02-03
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    """Plots average wind speed with errorbars."""
+
+    data = np.loadtxt("wind.dat")
+    t = data[:,0]
+    windspeed = data[:,1]
+    error = data[:,2]
+    
+    fig, ax = plt.subplots()
+    ax.errorbar(t, windspeed, error, marker='.', ls='', capsize=4)
+    ax.set_title("Average Wind Speed")
+    ax.minorticks_on()
+    ax.set_xlim([-1, 24])
+    ax.set_xticks(np.arange(0, 25, 4))
+    ax.set_ylim([0, 12])
+    ax.set_xlabel("Time [h]")
+    ax.set_ylabel("Wind Speed [kn]")
+    fig.savefig("plot.pdf")
+
+if __name__ == "__main__":
+    main()
