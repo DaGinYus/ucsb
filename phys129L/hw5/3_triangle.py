441d9bd0b6e4e16b943e8d737f0db64ba6de0cda
Matthew Wong <matthew@adveca.com>
2022-02-07 20:32:03 -0800

    initial upload of problem 3

---
+++ phys129L/hw5/3_triangle.py
@@ -0,0 +1,41 @@
+"""
+TKC!
+3-4-5 Right Triangle
+Matthew Wong
+Phys 129L Hw5 Pb3
+2022-02-10
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def main():
+    """Draws a 3-4-5 right triangle by setting individual pixels.
+
+    The thick lines are created by filling in the inside of the boundary
+    with a certain width. The image excluding the area inside the
+    boundary is then filled in with white pixel values to form the
+    background. This will cut off any overlap made by the width of the
+    lines.
+    """
+    X = 512 # width
+    Y = 512 # height
+    C = 0xFF # color
+    W = 1 # line weight
+    
+    # pixel values: x, y, color (in RGB)
+    pvals = np.zeros((X, Y, 3), dtype="uint8")
+    print(np.shape(pvals))
+
+    # divide up the triangle into 3 lines:
+    # the adjacent (base), opposite (height), and hypotenuse
+    # the hypotenuse is given by the equation y = (adj/opp)*x
+    width = 512
+    height = width*3//4
+    bounds = np.where(np.any(pvals[:,] < width, pvals[width,:] < height))
+    
+
+if __name__ == "__main__":
+    main()
