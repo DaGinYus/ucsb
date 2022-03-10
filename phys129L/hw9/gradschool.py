d162ac79dfd78c8244df3b720d19bdcacc5de009
Matthew Wong <matthew@adveca.com>
2022-03-09 22:24:44 -0800

    finished work on pb 2

--- phys129L/hw9/gradschool.py
+++ phys129L/hw9/gradschool.py
@@ -8,8 +8,10 @@ Phys 129L Hw9 Pb2
 
 
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy import integrate
 from scipy.stats import norm
+from scipy.special import erfc
 
 
 def gaussian(x, exp, std):
@@ -25,54 +27,91 @@ def gaussian(x, exp, std):
     return coeff * np.exp(arg)
 
 
-def est_prob(n, pop_size):
+def stdev(p, n):
+    """The standard deviation.
+
+    Args:
+        p: The probability of the event occuring.
+        n (int): The number of trials.
+    
+    Returns:
+        std: The standard deviation.
+    """
+    return np.sqrt(n*p*(1-p))
+
+
+def est_prob(s, pop_size):
     """Estimates the probability of a binomial distribution.
 
     The estimated probability from a single trial is obtained by
-    dividing n by the total population size. The standard deviation in
-    the probability is extrapolated from the standard deviation of the
+    dividing n by the total population size. Where n is the number of
+    'successes' in the initial trial.  The standard deviation in the
+    probability is extrapolated from the standard deviation of the
     binomial distribution.
 
     Args:
-        n (int): The number of 'successes'.
+        s (int): The number of 'successes'.
         pop_size (int): The total sample population.
 
     Returns:
         p_est: The estimated probability.
         p_std: The 1-sigma standard deviation in this probability.
     """
-    p_est = n / pop_size
-    variance = pop_size*p_est*(1 - p_est)
-    std = np.sqrt(variance)
-    p_std = (n + std)/pop_size - p_est
+    p_est = s / pop_size
+    std = stdev(p_est, pop_size)
+    p_std = (s + std)/pop_size - p_est
     return p_est, p_std
 
 
 def main():
     """Gradschool Admissions statistics."""
     # parameters defined from the problem
-    N = 1209
-    N_a = 143
-    p_est, p_std = est_prob(N_a, N)
+    tot_pop = 1209
+    tot_admit = 143
+    p_est, p_std = est_prob(tot_admit, tot_pop)
     print(f"Estimated Probability (p): {p_est}")
     # variance and stdev of this binomial distribution
-    var = N*p_est*(1 - p_est)
-    std_a = np.sqrt(var)
-    print(f"a) Standard Deviation: {std_a}")
-    # take (N_a +/- std_a) / N and subtract p to find the difference
-    # this is done in est_prob()
+    std_admit = stdev(p_est, tot_pop)
+    print(f"a) Standard Deviation: {std_admit}")
+    # the calculation is done in est_prob()
     print(f"b) One Sigma Deviation in p: {p_std}")
-    # Gaussian approximation
-    # expectation (loc) is N_a, stdev (scale) is std_a
-    gaussian = norm(loc=N_a, scale=std_a)
-    # calculations with 42 admitted out of 143
-    p_g_est, p_g_std = est_prob(42, 143)
+    # statistics for sample size 143
+    samp_pop = 143
+    samp_admit = 42
+    std = stdev(p_est, samp_pop)
+    # number of sigmas away using CDF of distribution
+    exp = samp_pop*p_est # expectation
+    sig = (samp_admit - exp)/std
+    p_samp = 0.5*erfc(sig/np.sqrt(2))
+    print(f"c) Chances of at least 42 students admitted: {p_samp}")
+    # statistics within the sample
+    p_g_est, p_g_std = est_prob(samp_admit, samp_pop)
+    std_samp = stdev(p_g_est, samp_pop)
     print(f"d) Estimated Probability (p_g): {p_g_est}\n"
           f"   One Sigma Deviation in p_g: {p_g_std}")
-    # calculations not in the group are just (143-42) out of 143
-    p_n_est, p_n_std = est_prob(101, 143)
+    # calculations not in the group are for N - group
+    excl_pop = tot_pop - samp_pop
+    excl_admit = tot_admit - samp_admit
+    p_n_est, p_n_std = est_prob(excl_admit, excl_pop)
+    std_excl = stdev(p_n_est, excl_pop)
     print(f"e) Estimated Probability (p_n): {p_n_est}\n"
           f"   One Sigma Deviation in p_n: {p_n_std}")
+    # plot the Gaussian distributions
+    norm_p = norm(loc=tot_admit, scale=std_admit)
+    norm_pg = norm(loc=samp_admit, scale=std_samp)
+    norm_pn = norm(loc=excl_admit, scale=std_excl)
+    scale_pg = samp_pop/tot_pop
+    scale_pn = excl_pop/tot_pop
+    x = np.linspace(0, 200, 1000)
+    plt.plot(x, norm_p.pdf(x), label="Total Applicant Pool")
+    plt.plot(x, norm_pg.pdf(x)*scale_pg, label="Pool of 143 Students")
+    plt.plot(x, norm_pn.pdf(x)*scale_pn, label="The Other Applicants")
+    plt.legend()
+    plt.title("Gaussian Distributions with Scaled Areas")
+    plt.xlabel("Number of Students Admitted")
+    plt.ylabel("Probability Density within Sample")
+    plt.savefig("gradschool.pdf", format="pdf")
+    plt.show()
 
 
 if __name__ == "__main__":
